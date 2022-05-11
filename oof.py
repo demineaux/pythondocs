3f52fb6173f3fb706732a85bffa6460543f34315
demineaux <33676309+demineaux@users.noreply.github.com>
2022-05-11 19:27:55 -0400

    Update and rename tori.py to oof.py

---
+++ oof.py
@@ -0,0 +1,5 @@
+user_name = input("What is your name? ")
+if user_name.lower() == "oof":
+    print("You have a oof name.")
+else:
+    print("Your name is ok.")
--- tori.py
+++
@@ -1,5 +0,0 @@
-user_name = input("What is your name? ")
-if user_name.lower() == "victoria":
-    print("You have a beautiful name.")
-else:
-    print("Your name is ok.")
